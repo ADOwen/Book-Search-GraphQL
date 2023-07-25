24f68c5409444afea3c7ceb32bf60da7a2730990
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-07-25 15:07:02 -0400

    Refactor auth middleware and add resolvers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const typeDefs = require('./schemas/typeDefs')
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./schemas/resolvers');
 const routes = require('./routes');
 
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers
 });
 
 
@@ -21,7 +24,9 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server,  {
+    context: authMiddleware
+  }));
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
